d0936aa90b0d99e5703f7c53c6e3bddb4c06b34a
Emily Watson <emilywatsonk@gmail.com>
2024-01-23 14:49:05 -0600

    added variables

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ console.log(
 );
 
 // Exercise 1
+let lastName = "Willingham";
+var age = 25;
 
 // Exercise 2
+const firstName = "Emily";
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5 
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
